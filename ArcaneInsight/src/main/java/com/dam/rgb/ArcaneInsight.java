6d9786f3493c4aefc51a0aac0a422143933c748c
Lunantisidhe <rebeca.gonzalez.balado@gmail.com>
2023-06-04 17:23:18 +0200

    eliminados logs info mongo

--- ArcaneInsight/src/main/java/com/dam/rgb/ArcaneInsight.java
+++ ArcaneInsight/src/main/java/com/dam/rgb/ArcaneInsight.java
@@ -4,10 +4,18 @@ import com.dam.rgb.db.BulkCardsImporter;
 import com.dam.rgb.menu.MenuManager;
 import com.dam.rgb.visual.Style;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.LoggerContext;
+import org.slf4j.LoggerFactory;
+
 // lanzador aplicacion
 public class ArcaneInsight {
 
     public static void main(String[] args) {
+
+        // elimina los logs de informacion de la base de datos en la consola
+        LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+        loggerContext.getLogger("org.mongodb.driver").setLevel(Level.ERROR);
 
         // impresion logo
         System.out.println(Style.LOGO);
