f7c83b0015a90f57ff33c26bcd35d0c3ac8ae7e0
Lunantisidhe <rebeca.gonzalez.balado@gmail.com>
2023-06-08 20:35:55 +0200

    añadido icono lnk

--- arcane_insight_app/app/application.bat
+++ arcane_insight_app/app/application.bat
@@ -1,4 +1,14 @@
 @echo off
+
+set "lnkFile=..\arcane_insight.lnk"
+set "exeFile=app\arcane_insight.exe"
+set "icoFile=..\img\arcane_insight.ico"
+
+echo [InternetShortcut] > "%lnkFile%"
+echo URL=file:///%exeFile% >> "%lnkFile%"
+echo IconFile=%icoFile% >> "%lnkFile%"
+echo IconIndex=0 >> "%lnkFile%"
+
 chcp 65001&cls
 cd /d "%~dp0"
 powershell.exe -Command "& '.\arcane_insight.exe'"
